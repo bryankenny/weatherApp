b8e18a429d21b18d7655d4902631435c9ead93dd
Bryan Kenny <b.93.kenny@gmail.com>
2019-05-22 20:36:06 +0000

    added a paramater to change the units to metric, added a message to return the weather info instead of the whole json object

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ let request = require('request');
 
 let apiKey = 'f0e5da68be2a36a711a30acd9710c548';
 let city = 'portland';
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 
 request(url, function (err, response, body) {
   if(err){
     console.log('error:', error);
   } else {
-    console.log('body:', body);
+    let weather = JSON.parse(body);
+    let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+    console.log(message);
   }
 });
 
