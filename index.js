2e19cccd2a04c2ae888756ebc257fc644c2279e5
Bryan Kenny <b.93.kenny@gmail.com>
2019-05-22 20:23:15 +0000

    A successful request to the openWeatherMaps api with a test city works

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-f0e5da68be2a36a711a30acd9710c548
+let request = require('request');
+
+let apiKey = 'f0e5da68be2a36a711a30acd9710c548';
+let city = 'portland';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+request(url, function (err, response, body) {
+  if(err){
+    console.log('error:', error);
+  } else {
+    console.log('body:', body);
+  }
+});
+
