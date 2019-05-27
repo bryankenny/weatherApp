d51514eb83a88e4060212240d19430b780d267da
Bryan Kenny <b.93.kenny@gmail.com>
2019-05-27 18:14:40 +0000

    added a css file within the punlic folder, app.use'd it in server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
+app.use(express.static('public'));;
 
 app.get('/', function (req, res) {
   res.render('index.ejs')
